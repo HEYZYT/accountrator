fba9ac1375cb4fc6bb8ae9fa4dfe190b42cba4fd
HEYZYT <43936414+HEYZYT@users.noreply.github.com>
2018-10-07 22:52:02 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,8 +4,8 @@ const bot = new DynamicsCompressorNode.Client();
 var prefix = ('*')
 
 bot.on('ready', function(){
-    bot.user.setGame('*help | AccountRator');
-    console.log('Connected!')
+    bot.user.setGame('*help);
+
 })
 
-bot.login("NDk4NTY0MjY3NzU1MTc1OTY2.DpvkGg.g2MsLWDHBxN96n7zwROH0-4McMw")
+bot.login('NDk4NTY0MjY3NzU1MTc1OTY2.DpvkGg.g2MsLWDHBxN96n7zwROH0-4McMw')
