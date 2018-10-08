807af3c5c90f64bcd0266504dd094ba12da5c2df
HEYZYT <43936414+HEYZYT@users.noreply.github.com>
2018-10-08 18:28:31 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,9 +4,9 @@ const bot = new DynamicsCompressorNode.Client();
 var prefix = ('*')
 
 bot.on('ready', function(){
-    bot.user.setGame('*help);
+    bot.user.setGame('*help');
 
 })
 
-bot.login('NDk4NTY0MjY3NzU1MTc1OTY2.DpvkGg.g2MsLWDHBxN96n7zwROH0-4McMw')
+bot.login('NDk4NTY0MjY3NzU1MTc1OTY2.DpvkGg.g2MsLWDHBxN96n7zwROH0-4McMw');
 
